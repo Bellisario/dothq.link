3909097aeae72b072e0afe5e05db94a9da58369f
Lunix <ramezjoseph8@gmail.com>
2021-02-01 01:16:03 +0200

    📝 Start script

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+// Configure Middleware
+
+// Configure Rutes
+
+// Start server
+app.listen(80, () => {
+    console.log("🌎 Server Started!")
+})
