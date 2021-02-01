eb341fda8400a45b5cd9f4037ed62eede59f16f6
Lunix <ramezjoseph8@gmail.com>
2021-02-01 12:37:26 +0200

    🚧 JSON based Urls

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
+const links = require('./links.json');
 
-// Configure Middleware
-
-// Configure Rutes
+// Configure Routes
+links.l.forEach(link => {
+    app.get(`/${link.slug}`, (req, res) => {
+        res.redirect(link.to)
+    })
+})
 
 // Start server
 app.listen(80, () => {
-    console.log("🌎 Server Started!")
+    console.log("Server started running.")
 })
