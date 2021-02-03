721500fecb02437fea9d485a56f17f5e9c82f884
Kieran <kieran@dothq.co>
2021-02-03 13:26:38 +0000

    ✨ Add about page to dothq.link

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ links.l.forEach(link => {
     })
 })
 
+app.get("/", (req, res) => {
+    res.send("<p>dothq.link the official link shortener for Dot HQ products, services and pages.<br/><br/>You should definitely join our Discord Server through our shortened URL, <a href='https://dothq.link/dsc'>dothq.link/dsc</a>.</p>")
+})
+
 // Start server
 app.listen(80, () => {
     console.log("Server started running.")
