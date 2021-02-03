4fb77b52acdba2945f872e8701b3e0b91db1d2a3
Bellisario <72039923+Bellisario@users.noreply.github.com>
2021-02-03 18:33:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const links = require('./links.json');
-const port = process.env.PORT || 3000;
 
 // Configure Routes
 links.l.forEach(link => {
@@ -14,7 +13,4 @@ app.get("/", (req, res) => {
     res.send("<p>dothq.link the official link shortener for Dot HQ products, services and pages.<br/><br/>You should definitely join our Discord Server through our shortened URL, <a href='https://dothq.link/dsc'>dothq.link/dsc</a>.</p>")
 })
 
-// Start server
-app.listen(port, () => {
-    console.log("Server started running.")
-})
+app.listen(process.env.PORT || 3000);//publish to heroku
